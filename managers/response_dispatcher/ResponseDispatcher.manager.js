e88df4790a5007afbe1d04a8ca87831fcea36b5f
sekams <spssekamatte@gmail.com>
2024-12-29 08:47:27 -0500

    mark dispatched responses as self-handled

--- managers/response_dispatcher/ResponseDispatcher.manager.js
+++ managers/response_dispatcher/ResponseDispatcher.manager.js
@@ -1,14 +1,17 @@
 module.exports = class ResponseDispatcher {
-    constructor(){
-        this.key = "responseDispatcher";
+    constructor() {
+        this.key = 'responseDispatcher';
     }
-    dispatch(res, {ok, data, code, errors, message, msg}){
-        let statusCode = code? code: (ok==true)?200:400;
-        return res.status(statusCode).send({
+    dispatch(res, args) {
+        const { ok, data, code, errors, message, msg } = args;
+        let statusCode = code ? code : ok == true ? 200 : 400;
+        const response = res.status(statusCode).send({
             ok: ok || false,
             data: data || {},
             errors: errors || [],
-            message: msg || message ||'',
+            message: msg || message || '',
         });
+        response.selfHandleResponse = true;
+        return response;
     }
-}
+};
